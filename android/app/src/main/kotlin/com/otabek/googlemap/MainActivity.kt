ea3577fd8e6fe08baee81930055edf3a6691a40b
Otabek66 <otabekotaboev@icloud.com>
2022-07-07 09:05:15 +0500

    yandex map succesfully added

--- android/app/src/main/kotlin/com/otabek/googlemap/MainActivity.kt
+++ android/app/src/main/kotlin/com/otabek/googlemap/MainActivity.kt
@@ -1,6 +1,15 @@
 package com.otabek.googlemap
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+import com.yandex.mapkit.MapKitFactory
 
 class MainActivity: FlutterActivity() {
+  override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+    MapKitFactory.setLocale("YOUR_LOCALE") // Your preferred language. Not required, defaults to system language
+    MapKitFactory.setApiKey("4807dbe1-0229-4a02-8361-ced2a730bc92") // Your generated API key
+    super.configureFlutterEngine(flutterEngine)
+  }
 }
---
+++ lib/screens/home_view.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:yandex_mapkit/yandex_mapkit.dart';
+
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
+
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: YandexMap(),
+    );
+  }
+}
